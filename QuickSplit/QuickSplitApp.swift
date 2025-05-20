4ae8a40a60c559d3627637de7318047c6537d4b3
Jason Mai <maijason112102@gmail.com>
2025-05-20 10:05:17 -0400

    Initial Commit

---
+++ QuickSplit/QuickSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSplitApp.swift
+//  QuickSplit
+//
+//  Created by Jason Mai on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickSplitTests/QuickSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSplitTests.swift
+//  QuickSplitTests
+//
+//  Created by Jason Mai on 5/20/25.
+//
+
+import Testing
+@testable import QuickSplit
+
+struct QuickSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
